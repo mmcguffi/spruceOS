c04ae08f1a201f14a9938202f5fcc2be64dbfe9d
Matt McGuffie <mmcguffi@gmail.com>
2026-06-07 08:39:54 -0700

    Fix SD card read-only recovery

--- App/PyUI/main-ui/devices/device_common.py
+++ App/PyUI/main-ui/devices/device_common.py
@@ -499,6 +499,12 @@ class DeviceCommon(AbstractDevice):
 
     def perform_sdcard_ro_check(self):
         if self.is_filesystem_read_only("/mnt/SDCARD"):
+            subprocess.run(
+                ["mount", "-o", "remount,rw", "/dev/mmcblk1p1", "/mnt/SDCARD"],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL
+            )
+        if self.is_filesystem_read_only("/mnt/SDCARD"):
             Display.display_message("Warning: /mnt/SDCARD is read-only. Please check your SD card.", duration_ms=10000)
 
     def sync_hw_clock(self):
